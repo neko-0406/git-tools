b3bfe7e379da367f1240b673f45b3a6c02ddf54e
ryoya <sateraito3696@gmail.com>
2025-12-17 12:15:15 +0900

    環境変数読み込みを追加

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,17 @@ use ratatui::{DefaultTerminal, Frame, prelude, widgets::Widget};
 
 #[derive(Default)]
 pub struct App {
-    exit: bool
+    github_token: String,
+    exit: bool,
 }
 
 impl App {
     pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        // 環境変数の読み込み
+        dotenv::dotenv().ok();
+        // GitHubのアクセストークン取得・無ければアプリを落とす
+        self.github_token =
+            dotenv::var("GITHUB_TOKEN").expect("GitHubへのアクセストークンが見つかりませんでした");
         while !self.exit {
             terminal.draw(|frame| self.draw(frame))?;
             self.handle_events();
@@ -19,14 +25,10 @@ impl App {
     fn draw(&self, frame: &mut Frame) {
         frame.render_widget(self, frame.area());
     }
- 
+
     fn handle_events(&mut self) {}
 }
 
 impl Widget for &App {
-    fn render(self, area: prelude::Rect, buf: &mut prelude::Buffer)
-        where
-            Self: Sized {
-        
-    }
+    fn render(self, area: prelude::Rect, buf: &mut prelude::Buffer) {}
 }
